fe3e6cd91059d95bf9c27def07bd81dbba40ee67
Adem Onur Çelik <ademonurcelik@icloud.com>
2023-10-25 22:24:06 +0300

    feat: basic client and server communication

--- client.cpp
+++ client.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
 
 int main()
 {
+    // Create a socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Set up server address information
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);                   // Port number
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Server's IP address
+
+    // Connect to the server
+    connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+
+    std::cout << "Connecting to server..." << std::endl;
+
+    char buffer[4096];
+    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+    if (bytesRead <= 0)
+    {
+        std::cerr << "Failed to receive response from server." << std::endl;
+    }
+    else
+    {
+        buffer[bytesRead] = '\0';
+        std::cout << buffer << std::endl;
+        std::cout << "You've successfully connected." << std::endl;
+    }
+
+    // Close the client socket
+    close(clientSocket);
 
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 int main()
 {
+    // Create a socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    std::cout << "Socket " << serverSocket << " created." << std::endl;
+
+    // Set up server address information
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8080);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket to the server address
+    bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+
+    // Listen for incoming connections
+    listen(serverSocket, 10);
+
+    std::cout << "Server is listening on port 8080..." << std::endl;
+
+    while (true)
+    {
+        // Accept a client connection
+        sockaddr_in clientAddr;
+        socklen_t clientAddrSize = sizeof(clientAddr);
+        int clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrSize);
+
+        std::cout << "Received connection request from " << inet_ntoa(clientAddr.sin_addr) << ":" << clientSocket << std::endl;
+
+        // Grant the request and send a success message
+        send(clientSocket, "SYSTEM | 200 | Successful connection.", 38, 0);
+        std::cout << "Request granted." << std::endl;
+        std::cout << "SYSTEM | 200 | Successful connection." << std::endl;
+
+        // Close the client socket
+        close(clientSocket);
+    }
+
+    // Close the server socket
+    close(serverSocket);
+
     return 0;
 }
